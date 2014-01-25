18529177f7d4bd6f330c134a66caccecd7d4937b
Christoph Becher <christoph.becher@innogames.de>
2014-01-25 12:45:19 +0100

    update

--- Library/Game/Dialog.cs
+++ Library/Game/Dialog.cs
@@ -3,44 +3,41 @@ using UnityEngine;
 
 namespace Library
 {
-	public class Dialog : MonoBehaviour
-	{
-		protected DialogScene DialogScene { get; set; }
+    public class Dialog : MonoBehaviour
+    {
+        protected DialogScene DialogScene { get; set; }
 
-		public void Awake()
-		{
-			EventBus.Register(Events.DIALOG_LOAD, LoadDialog);
-		}
+        public void Awake()
+        {
+            EventBus.Register(Events.DIALOG_LOAD, LoadDialog);
+        }
 
-		public void LoadDialog(object data)
-		{
-			int dialog = (int)data;
+        public void LoadDialog(object data)
+        {
+            int dialog = (int)data;
+            DialogScene = Serializer.Deserialize<DialogScene>("DialogScene_" + dialog + ".xml");
+        }
 
-			DialogScene = Serializer.Deserialize<DialogScene>("DialogScene_" + dialog + ".xml");
+        public void OnGUI()
+        {
+            GUI.Label(new Rect(100, 100, 300, 300), DialogScene.GetCurrent().GetCurrent());
+        }
 
-			Debug.Log(DialogScene.DialogTexts.Count);
-		}
+        public void OnDestroy()
+        {
+            EventBus.Unregister(Events.DIALOG_LOAD, LoadDialog);
+        }
 
-		public void OnGUI()
-		{
-			GUI.Label(new Rect(100, 100, 300, 300), DialogScene.GetCurrent().GetCurrent());
-		}
-
-		public void OnDestroy()
-		{
-			EventBus.Unregister(Events.DIALOG_LOAD, LoadDialog);
-		}
-
-		public void Update()
-		{
-			if (Input.GetKeyDown(KeyCode.Return))
-			{
-				if (!DialogScene.GetCurrent().SetNext())
-				{
-					DialogScene.SetNext();
-				}
-			}
-		}
-	}
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                if (!DialogScene.GetCurrent().SetNext())
+                {
+                    DialogScene.SetNext();
+                }
+            }
+        }
+    }
 }
 
