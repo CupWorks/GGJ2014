1b8c5a6c5d2619dae489f1d887371c6c5f49dac8
Christoph Becher <christoph.becher@innogames.de>
2014-01-26 11:34:06 +0100

    update

--- Library/Core/Events.cs
+++ Library/Core/Events.cs
@@ -2,15 +2,15 @@ using System;
 
 namespace Library
 {
-    public class Events
-    {
-        public const int DIALOG_LOAD = 100;
-        public const int SET_NEXT_SCENE = 110;
-        public const int CHANGE_SCENE = 10000;
-        public const int CREATE_OBJECT = 2000;
-        public const int PLAY_SOUND = 3000;
-        public const int SET_IMAGE = 1337;
-        public const int LOAD_LEVEL = 1200;
-    }
+	public class Events
+	{
+		public const int DIALOG_LOAD = 100;
+		public const int SET_NEXT_SCENE = 110;
+		public const int CHANGE_SCENE = 10000;
+		public const int CREATE_OBJECT = 2000;
+		public const int PLAY_SOUND = 3000;
+		public const int SET_IMAGE = 1337;
+		public const int LEVEL_COMPLETED = 1200;
+	}
 }
 
--- Library/Game/Play.cs
+++ Library/Game/Play.cs
@@ -5,8 +5,16 @@ namespace Library
 {
 	public class Play : MonoBehaviour
 	{
+		protected int NextScene { get; set; }
+
 		public void Awake()
 		{
+			EventBus.Register(Events.SET_NEXT_SCENE, SetNextScene);
+		}
+
+		public void SetNextScene(object data)
+		{
+			NextScene = (int)data;
 		}
 	}
 }
